29f7745425b312815fb520996c304332cb1e8157
Doug Dennis <doug.dennis@pnnl.gov>
2024-03-02 19:41:11 -0800

    added descriptions to keymaps, added live_grep keymap, and changed grep_string keymap

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,16 @@
 local lsp = require("lsp-zero")
 
 lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { buffer = bufnr, remap = false, desc = "Go to definition." })
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = bufnr, remap = false, desc = "Display LSP hover." })
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, { buffer = bufnr, remap = false, desc = "View workspace symbol." })
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, { buffer = bufnr, remap = false, desc = "Open diagnostic float." })
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, { buffer = bufnr, remap = false, desc = "Go to next diagnostic message." })
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, { buffer = bufnr, remap = false, desc = "Go to previous diagnostic message." })
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { buffer = bufnr, remap = false, desc = "View and execute available code actions." })
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { buffer = bufnr, remap = false, desc = "View the references of the current symbol." })
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { buffer = bufnr, remap = false, desc = "Rename the current symbol." })
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = false, desc = "Display signature help." })
 end)
 
 require("mason").setup({})
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>ps", function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files in workspace." })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search git files." })
+vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "Grep for the string under the cursor." })
+vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Activate live grep in Telescope" })
+
